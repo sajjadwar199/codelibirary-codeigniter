01cec54092a042c5c16c37106e5b42a6d40b8c3f
sajjadkareem <ggh_ggh.1998@yahoo.com>
2022-02-26 14:03:31 +0300

    commit message

---
+++ skCrudModalAjax/skCrudModalAjax.js
@@ -0,0 +1,220 @@
+ /* for  edit data */
+ function edit(edit_url, modal_id, edit_save_btn_id) {
+ 	$(document).on('click', '.update_btn', function () {
+ 		var id = $(this).attr("id");
+ 		$.ajax({
+ 			url: edit_url,
+ 			method: "POST",
+ 			data: {
+ 				id: id
+ 			},
+ 			dataType: "json",
+ 			cache: false,
+ 			success: function (dataResult) {
+
+ 				/* setting st art */
+ 				$('#username_edit').val(dataResult[0].username);
+ 				$('#password_edit').val(dataResult[0].password);
+ 				$('#validity_edit').val(dataResult[0].validity);
+ 				/* for select options edit */
+ 				if (dataResult[0].validity == 1) {
+ 					$('select option:contains("مستخدم عادي")').removeAttr('selected');
+ 					$('select option:contains("مدير")').attr('selected', 'selected');
+ 				} else {
+ 					$('select option:contains("مدير")').removeAttr('selected');
+ 					$('select option:contains("مستخدم عادي ")').attr('selected', 'selected');
+ 				}
+ 				$('#' + modal_id).modal('show');
+ 				//   $('.modal-title').text("Edit User");  
+ 				$('#' + edit_save_btn_id).val(id);
+ 				/* setting end */
+
+ 			}
+ 		})
+ 	});
+ }
+ /* for showing data   */
+ function serverSide_datatable(url, table_id) {
+ 	$(document).ready(function () {
+ 		$("#" + table_id).DataTable({
+ 			retrieve: true,
+ 			"processing": !0,
+ 			"ajax": {
+ 				url: url,
+ 				type: 'GET',
+ 			},
+ 			dom: 'Blfrtip',
+ 			buttons: [
+ 				'excel', 'print'
+ 			],
+ 			"language": {
+ 				"sProcessing": "جارٍ التحميل...",
+ 				"sLengthMenu": "أظهر _MENU_ مدخلات",
+ 				"sZeroRecords": "لم يعثر على أية سجلات",
+ 				"sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
+ 				"sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+ 				"sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+ 				"sInfoPostFix": "",
+ 				"sSearch": "ابحث:",
+ 				"sUrl": "",
+ 				"oPaginate": {
+ 					"sFirst": "الأول",
+ 					"sPrevious": "السابق",
+ 					"sNext": "التالي",
+ 					"sLast": "الأخير"
+ 				},
+ 				"buttons": {
+ 					"print": "طباعة",
+ 					"copyKeys": "زر <i>ctrl<\/i> أو <i>⌘<\/i> + <i>C<\/i> من الجدول<br>ليتم نسخها إلى الحافظة<br><br>للإلغاء اضغط على الرسالة أو اضغط على زر الخروج.",
+ 					"copySuccess": {
+ 						"_": "%d قيمة نسخت",
+ 						"1": "1 قيمة نسخت"
+ 					},
+ 					"pageLength": {
+ 						"-1": "اظهار الكل",
+ 						"_": "إظهار %d أسطر"
+ 					},
+ 					"collection": "مجموعة",
+ 					"copy": "نسخ",
+ 					"copyTitle": "نسخ إلى الحافظة",
+ 					"csv": "CSV",
+ 					"excel": "اكسل",
+ 					"pdf": "PDF",
+ 					"colvis": "إظهار الأعمدة",
+ 					"colvisRestore": "إستعادة العرض"
+ 				},
+ 			}
+ 		}, )
+ 	});
+ }
+ //for insert data
+ function insertData(modal_id, insert_url, btn_add_id, showdata_url, table_id, messages = {
+ 	success: 'Data added successfully !',
+ 	error: "Error occured !"
+ }) {
+ 	$(document).ready(function () {
+ 		$('#' + btn_add_id).on('click', function () {
+ 			var username = $('#username').val();
+ 			var password = $('#password').val();
+ 			var validity = $('#validity').val();
+ 			if (username != "" && password != "" && validity != "") {
+ 				$("#" + btn_add_id).attr("disabled", "disabled");
+ 				$.ajax({
+ 					url: insert_url,
+ 					type: "POST",
+ 					data: {
+ 						username: $('#username').val(),
+ 						password: $('#password').val(),
+ 						validity: $('#validity').val(),
+ 					},
+ 					cache: false,
+ 					success: function (dataResult) {
+ 						if (dataResult.success != false) {
+ 							alert
+ 								(messages.success);
+ 						}
+ 						$('#' + table_id).DataTable().destroy();
+ 						serverSide_datatable(showdata_url, table_id);
+ 						$('#validity').val("");
+ 						$('#password').val("");
+ 						$('#username').val("");
+ 						$("#" + btn_add_id).removeAttr("disabled");
+ 						var dataResult = JSON.parse(dataResult);
+ 						if (dataResult.statusCode == 200) {
+ 							// $("#"+form_id).find('input:text').val('');
+ 							$("#" + success_div_id).html(dataResult);
+ 						} else if (dataResult.statusCode == 201) {
+ 							alert
+ 								(messages.error);
+ 						}
+ 					}
+ 				});
+ 			}
+ 			//     else {
+ 			//         alert('Please fill all the field !');
+ 			//  }
+ 		});
+ 	});
+ }
+ /* for update data  */
+ function updateData(edit_modal_id, insert_url, edit_add_id, showdata_url, table_id, messages = {
+ 	success: 'Data updated successfully !',
+ 	error: "Error occured !"
+ }) {
+ 	$('#' + edit_add_id).on('click', function () {
+ 		var username = $('#username_edit').val();
+ 		var password = $('#password_edit').val();
+ 		var validity = $('#validity_edit').val();
+ 		if (username != "" && password != "" && validity != "") {
+ 			$("#" + edit_add_id).attr("disabled", "disabled");
+ 			$.ajax({
+ 				url: insert_url,
+ 				type: "POST",
+ 				data: {
+ 					username: $('#username_edit').val(),
+ 					password: $('#password_edit').val(),
+ 					validity: $('#validity_edit').val(),
+ 					id: $('#' + edit_add_id).val()
+ 				},
+ 				cache: false,
+ 				success: function (dataResult) {
+ 					if (dataResult.success != false) {
+ 						alert
+ 							(messages.success);
+ 					}
+ 					$('#' + table_id).DataTable().destroy();
+ 					serverSide_datatable(showdata_url, table_id);
+ 					$('#validity_edit').val("");
+ 					$('#password_edit').val("");
+ 					$('#username_edit').val("");
+ 					$('#' + edit_modal_id).modal('hide');
+
+ 					$("#" + edit_add_id).removeAttr("disabled");
+ 					var dataResult = JSON.parse(dataResult);
+ 					if (dataResult.statusCode == 200) {
+ 						$("#" + success_div_id).html(dataResult);
+ 					} else if (dataResult.statusCode == 201) {
+ 						alert
+ 							(messages.error);
+ 					}
+ 				}
+ 			});
+ 		}
+ 		//     else {
+ 		//         alert('Please fill all the field !');
+ 		//  }
+ 	});
+ }
+
+
+ /* FOR delete data */
+
+ function deleteData(delete_url,showdata_url, table_id, messages = {
+	success: 'Data deleted successfully !',
+	error: "Error occured !"
+,
+confirmdelete:"هل أنت متأكد من الحذف"
+}) {
+	$(document).on('click', '.delete_btn', function(){  
+		var id = $(this).attr("id");  
+		if(confirm(messages.confirmdelete))  
+		{  
+			 $.ajax({  
+				  url:delete_url,  
+				  method:"POST",  
+				  data:{id:id},  
+				  success:function(data)  
+				  {  
+					alert(messages.success);  
+
+					$('#' + table_id).DataTable().destroy();
+					serverSide_datatable(showdata_url, table_id);
+ 				  }  
+			 });  
+		}  
+		else  
+		{  
+			 return false;       
+		}  
+   });  
+ }
---
+++ skCrudModalAjax/skCrudModalAjax.php
@@ -0,0 +1,174 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ *    create crud with modalBootstrap and ajax
+ * @package skLibiraris
+ * @author sajjadkareem 
+ * @category modalAjaxCrud
+ * 
+ * 
+ */
+class skCrudModalAjax  extends CI_Controller
+{
+     public   $ModelClassName = "manageUsersModel";
+    public   $validation = array(
+        array(
+            'field' => 'username',
+            'label' => 'أسم المستخدم ',
+            'rules' => 'required',
+            'errors' => array(
+                'required' => 'الرجاء ملء حقل %s.',
+            ),
+        ),
+        array(
+            'field' => 'password',
+            'label' => ' كلمة المرور ',
+            'rules' => 'required',
+            'errors' => array(
+                'required' => 'الرجاء ملء حقل %s.',
+            ),
+        ),
+        array(
+            'field' => 'validity',
+            'label' => ' الصلاحية ',
+            'rules' => 'required',
+            'errors' => array(
+                'required' => 'الرجاء أختيار   %s.',
+            ),
+        )
+    );
+    public function __construct()
+    {
+        parent::__construct();
+        // import model 
+        $this->load->model($this->ModelClassName, "ModelName");
+        $this->load->library('form_validation');
+    }
+    public function set_show_data($r)
+    {
+        return  array(
+            $r->username,
+            $r->password,
+            $r->validity,
+            "<a      class='btn btn-success update_btn' id=" . $r->id .  ">تعديل</a>",
+            "<a  class='btn btn-danger delete_btn' id=" .  $r->id . ">حذف</a>",
+        );
+    }
+    public function index()
+    {
+        $this->load->view("dashbord_inc/header.php");
+        $this->load->view("pages/importExcel.php");
+        $this->load->view("dashbord_inc/footer.php");
+    }
+    /**
+     * insert لأضافة البيانات 
+     *
+     * @return void
+     */
+    public function insert()
+    {
+        // set data
+        $data = array(
+            "username" => $this->input->post("username"),
+            "password" => $this->input->post("password"),
+            "validity" => $this->input->post("validity")
+        );
+        //validation 
+        $this->form_validation->set_rules($this->validation);
+        if ($this->form_validation->run() != false) {
+            // insert 
+            $res = $this->ModelName->insert($data);
+            json_encode($res);
+        } else {
+            echo json_encode(['success' => false]);
+        }
+    }
+    /**
+     * edit    لعرض العنصر المراد تعديله
+     *
+     * @param  mixed $id
+     * @return void
+     * 
+     * 
+     * 
+     */
+    public function edit()
+    {
+        $id = $this->input->post("id");
+        $data = $this->ModelName->get_where($id);
+        echo json_encode($data);
+    }
+    /**
+     * update  لتعديل البيانات
+     *
+     * @return void
+     */
+    public function update()
+    {
+        $data = array(
+            "id" => $this->input->post("id"),
+            "username" => $this->input->post("username"),
+            "password" => $this->input->post("password"),
+            "validity" => $this->input->post("validity")
+        );
+        $res = $this->ModelName->update($data);
+        json_encode($res);
+        echo $this->input->post("id");
+    }
+    /**
+     * delete لحذف البيانات
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function delete()
+    { 
+        $id=$this->input->post("id");
+        $delet = $this->ModelName->delete($id);
+        echo json_encode($delet);
+    }
+    /**
+     * show لعرض البيانات 
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function show($id)
+    {
+        $show =  $this->ModelName->get_where($id);
+        json_encode($show);
+    }
+    public  function set_show_data_ajax()
+    {
+        $model = new $this->ModelName;
+        $model_data = $model->get_data();
+        $data = array();
+        foreach ($model_data as $r) {
+            $data[] = $this->set_show_data($r);
+        }
+        return $data;
+    }
+    /**
+     * List لعرض البيانات وتحويلها الى json
+     *
+     * @return void
+     */
+    public function List()
+    {
+        // Datatables Variables
+        $model = new $this->ModelName;
+        $draw = intval($this->input->get("draw"));
+        $start = intval($this->input->get("start"));
+        $length = intval($this->input->get("length"));
+        $output = array(
+            "draw" => $draw,
+            "recordsTotal" => $model->get_count(),
+            "recordsFiltered" => $model->get_count(),
+            "data" => $this->set_show_data_ajax()
+        );
+        echo json_encode($output);
+        exit();
+        $data = $this->ModelName->get_data();
+        json_encode($data);
+    }
+}
